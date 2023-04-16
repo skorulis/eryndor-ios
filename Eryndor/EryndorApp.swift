1a12ca6ad4c365f7fbb6daa9947702d5386cfa13
Alex Skorulis <skorulis@gmail.com>
2023-04-16 13:23:35 +1000

    Initial Commit

---
+++ Eryndor/EryndorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EryndorApp.swift
+//  Eryndor
+//
+//  Created by Alexander Skorulis on 16/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct EryndorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

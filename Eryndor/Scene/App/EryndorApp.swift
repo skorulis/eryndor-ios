d9271fc77361e83848f84b60962dca3af08a4126
Alex Skorulis <skorulis@gmail.com>
2023-04-16 13:28:55 +1000

    Initial project setup

--- Eryndor/Scene/App/EryndorApp.swift
+++ Eryndor/Scene/App/EryndorApp.swift
@@ -1,17 +1,16 @@
-//
-//  EryndorApp.swift
-//  Eryndor
-//
 //  Created by Alexander Skorulis on 16/4/2023.
-//
 
 import SwiftUI
 
 @main
 struct EryndorApp: App {
+    
+    private let ioc = IOC(purpose: .normal)
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.factory, ioc)
         }
     }
 }
---
+++ Eryndor/Service/App/IOC.swift
@@ -0,0 +1,13 @@
+//Created by Alexander Skorulis on 16/4/2023.
+
+import ASKCore
+import Foundation
+
+final class IOC: IOCService {
+    
+    override init(purpose: IOCPurpose = .testing) {
+        super.init(purpose: purpose)
+        
+    }
+    
+}
